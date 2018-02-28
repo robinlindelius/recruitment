63baff8ed8adc81a7276a143676da340c2cd7e13
rodilf <lucaswallen@hotmail.com>
2018-02-28 16:03:09 +0100

    Connected user and person in account creation and database.

--- src/main/java/se/kth/iv1201/recruitment/controller/RegisterController.java
+++ src/main/java/se/kth/iv1201/recruitment/controller/RegisterController.java
@@ -3,10 +3,10 @@ package se.kth.iv1201.recruitment.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import se.kth.iv1201.recruitment.entity.Person;
 import se.kth.iv1201.recruitment.entity.User;
 import se.kth.iv1201.recruitment.entity.UserRole;
 import se.kth.iv1201.recruitment.repository.UserRepository;
@@ -20,22 +20,30 @@ public class RegisterController {
 
     @GetMapping("/register")
     public String getRegister(Model model){
+        Person person = new Person();
         User user = new User();
+        //user.setPerson(person);
+        //user.setEnabled(true);
+        //user.addRole(UserRole.APPLICANT);
+
         model.addAttribute("user", user);
+        model.addAttribute("person", person);
         return "register";
     }
 
     @PostMapping(path="/register")
-    public String postRegister(@Valid User user, BindingResult result) {
+    public String postRegister(@Valid User user, BindingResult bindingResultUser,
+                               @Valid Person person, BindingResult bindingResultPerson) {
 
-        user.setEnabled(true);
-        user.addRole(UserRole.APPLICANT);
-
-        if(result.hasErrors()) {
+        if(bindingResultUser.hasErrors() || bindingResultPerson.hasErrors()) {
+            System.out.println("REGISTER ERROR");
             return "register";
         }
+        user.setEnabled(true);
+        user.addRole(UserRole.APPLICANT);
+        user.setPerson(person);
 
         userRepository.save(user);
-        return "redirect:/login?reg=success";
+        return "home";
     }
 }
