6c1de4c88403ec81faaf6d3bb796a44aacca75a5
samuelagdelius <samuelag@kth.se>
2018-03-07 13:14:15 +0100

    Added the support for deployment of final product

--- src/main/java/se/kth/iv1201/recruitment/Application.java
+++ src/main/java/se/kth/iv1201/recruitment/Application.java
@@ -2,17 +2,24 @@ package se.kth.iv1201.recruitment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
- * Starts the application
+ * Extends SpringBootServletInitializer to include
+ * support for WAR deployment
  */
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     /**
      * Starts the application
      */
-    public static void main(String[] args) throws Throwable {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
 
